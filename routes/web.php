19453d94b764969ee3c7c681872be22ccc7d580d
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-04-30 13:03:50 +0700

    feat: initialize web routes for authentication, dashboard, and resource management modules

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
 Route::get('/terms', function () {
